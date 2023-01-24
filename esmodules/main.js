718f71a02dbb08ed01260765f76e728737435a27
Gerrit Beine <mail@gerritbeine.com>
2023-01-24 22:47:34 +0100

    JavaScript Modul erstellt
    Minimales JavaScript Modul für Initialisierungsaufgaben…

---
+++ esmodules/main.js
@@ -0,0 +1,9 @@
+const moduleID = "pf2e-aos";
+
+Hooks.once("init", () => {
+    console.log("PF2E AoS | Aventurien on Steroids wird initialsiert.");
+});
+
+Hooks.on("ready", function() {
+    console.log("PF2E AoS | Aventurien on Steroids wird konfiguriert.");
+});
